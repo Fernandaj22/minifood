b2d1a7fed57f02f9b3356c93f70c15dd110513ad
Unknown <mjuarez22@alumnos.uaq.mx>
2017-10-25 16:37:03 -0500

    Todos los archivos 25102017

---
+++ js/funciones.js
@@ -0,0 +1,30 @@
+function mostrarmas(){
+	document.querySelector('.masopciones').classList.toggle('veropciones');
+	document.querySelector('.header').classList.toggle('headermostmas');
+	document.querySelector('.contenedor').classList.toggle('contmin');
+	alert("Hola")
+}
+function desplegarAlertas(){
+	document.querySelector('.alertas_des').classList.toggle('veralertas');
+}
+function desplegarOpciones(){
+	document.querySelector('.opciones').classList.toggle('ver-opciones');
+}
+function tablarecientes(){
+	document.querySelector('.recientes').classList.remove('ver-recientes');
+	document.querySelector('.opciones').classList.remove('ver-opciones');
+	document.querySelector('.enproceso').classList.remove('ver-enproceso');
+	document.querySelector('.entregados').classList.remove('ver-entregados');
+}
+function tablaproceso(){
+	document.querySelector('.enproceso').classList.add('ver-enproceso');
+	document.querySelector('.recientes').classList.add('ver-recientes');
+	document.querySelector('.opciones').classList.remove('ver-opciones');
+	document.querySelector('.entregados').classList.remove('ver-entregados');
+}
+function tablaentregados(){
+	document.querySelector('.entregados').classList.add('ver-entregados');
+	document.querySelector('.recientes').classList.add('ver-recientes');
+	document.querySelector('.opciones').classList.remove('ver-opciones');
+	document.querySelector('.enproceso').classList.remove('ver-enproceso');
+}
