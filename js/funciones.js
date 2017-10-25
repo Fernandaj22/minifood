5c026071366b79cb3482b3db9bd7f334562fe653
Unknown <mjuarez22@alumnos.uaq.mx>
2017-10-25 16:54:15 -0500

    Select
    Nombre de la seccion en que te encuentras

--- js/funciones.js
+++ js/funciones.js
@@ -15,16 +15,19 @@ function tablarecientes(){
 	document.querySelector('.opciones').classList.remove('ver-opciones');
 	document.querySelector('.enproceso').classList.remove('ver-enproceso');
 	document.querySelector('.entregados').classList.remove('ver-entregados');
+	document.getElementById("idn").innerHTML = "Pedidos recientes";
 }
 function tablaproceso(){
 	document.querySelector('.enproceso').classList.add('ver-enproceso');
 	document.querySelector('.recientes').classList.add('ver-recientes');
 	document.querySelector('.opciones').classList.remove('ver-opciones');
 	document.querySelector('.entregados').classList.remove('ver-entregados');
+	document.getElementById("idn").innerHTML = "Pedidos en proceso";
 }
 function tablaentregados(){
 	document.querySelector('.entregados').classList.add('ver-entregados');
 	document.querySelector('.recientes').classList.add('ver-recientes');
 	document.querySelector('.opciones').classList.remove('ver-opciones');
 	document.querySelector('.enproceso').classList.remove('ver-enproceso');
+	document.getElementById("idn").innerHTML = "Pedidos entregados";
 }
